003e7af90e3deb4dc0a5f18cb1b061ec2594881b
farisrazzan <farisrazzan@gmail.com>
2025-11-10 15:59:48 +0800

    feat: manage user

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,25 @@
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\ExplorationController;
+use App\Http\Controllers\User\UserController as ProfileController;
 
-// === Halaman Utama ===
-Route::get('/', [App\Http\Controllers\ExplorationController::class, 'index'])->name('exploration');
-Route::resource('eksplorasi', App\Http\Controllers\ExplorationController::class)
-    ->except(['create', 'edit', 'update', 'destroy']);
+Route::get('/', [ExplorationController::class, 'index'])->name('exploration');
+Route::resource('eksplorasi', ExplorationController::class)->except(['create', 'edit', 'update', 'destroy']);
 
 Auth::routes();
 
-// === Area User (butuh login) ===
 Route::middleware(['isLogin'])->group(function () {
-    Route::post('eksplorasi/like/{id}', [App\Http\Controllers\ExplorationController::class, 'like'])->name('eksplorasi.like');
-    Route::post('eksplorasi/comment/{id}', [App\Http\Controllers\ExplorationController::class, 'comment'])->name('eksplorasi.comment');
+    Route::post('eksplorasi/like/{id}', [ExplorationController::class, 'like'])->name('eksplorasi.like');
+    Route::post('eksplorasi/comment/{id}', [ExplorationController::class, 'comment'])->name('eksplorasi.comment');
 
     Route::resource('profile', App\Http\Controllers\User\ArtworkController::class)->except(['create', 'show', 'edit', 'update']);
-    Route::get('user/edit-profile', [App\Http\Controllers\User\UserController::class, 'index'])->name('edit.profile');
-    Route::put('user/update', [App\Http\Controllers\User\UserController::class, 'update'])->name('update.profile');
-    Route::put('user/update-photo', [App\Http\Controllers\User\UserController::class, 'updateProfile'])->name('update.profile.photo');
+    Route::get('user/edit-profile', [ProfileController::class, 'index'])->name('edit.profile');
+    Route::put('user/update', [ProfileController::class, 'update'])->name('update.profile');
+    Route::put('user/update-photo', [ProfileController::class, 'updateProfile'])->name('update.profile.photo');
 });
+
 
 // === Area Admin ===
 Route::prefix('dashboard')->group(function () {
@@ -27,9 +28,8 @@ Route::prefix('dashboard')->group(function () {
         return view('pages.admin.dashboard');
     })->name('admin.dashboard');
 
-    Route::get('/users', function () {
-        return view('pages.admin.users');
-    })->name('admin.users');
+    Route::get('/users', [UserController::class, 'index'])->name('admin.users');
+    Route::delete('/users/{id}', [UserController::class, 'destroy'])->name('admin.users.destroy');
 
     Route::get('/post', function () {
         return view('pages.admin.post');
@@ -39,4 +39,3 @@ Route::prefix('dashboard')->group(function () {
         return view('pages.admin.categories');
     })->name('admin.categories');
 });
-
